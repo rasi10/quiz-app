12b29d49ee8d422eb31e7e5872946de15f19677f
Rafael Silva <rafael@Rafaels-MacBook-Pro.local>
2020-04-08 00:27:57 +0200

    Controlling the end of the quiz

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
 const http = new EasyHTTP
 const ui = new UI
-let urlToFetch = 'http://vhost3.lnu.se:20080/question/21'
+let urlToFetch = ''
+let urlToSendAnswer = ''
 
-document.querySelector('#button1').addEventListener('click', fetchFirstQuestion)
+document.querySelector('#button1').addEventListener('click', startQuiz)
 document.querySelector('#button2').addEventListener('click', submitAnswer)
 
+function startQuiz() {
+    urlToFetch = 'http://vhost3.lnu.se:20080/question/1'
+    fetchQuestion(urlToFetch)
+}
 
-function fetchFirstQuestion() {
-    http.get(urlToFetch)
-        .then(data => ui.draw(data))
+function fetchQuestion(url) {
+    console.log(url)
+    http.get(url)
+        .then(data => {
+            ui.draw(data)
+            console.log(data)
+            urlToFetch = data.nextURL
+        })
         .catch(err => console.log(err))
 }
 
-function fetchNextQuestion(url){
-    console.log(url)
-    http.get(url)
-    .then(data => ui.draw(data))
-    .catch(err => console.log(err))
-}
-
-function submitAnswer(){
-    answerSubmitted = document.querySelector('input:checked').getAttribute('value')
+function submitAnswer() {
+    if (document.querySelector('#answer-text')) {
+        answerSubmitted = document.getElementById('answer-text').value
+    } else {
+        answerSubmitted = document.querySelector('input:checked').getAttribute('value')
+    }
     answerData = { answer: answerSubmitted }
     console.log(answerData)
 
-    http.post('http://vhost3.lnu.se:20080/answer/21',answerData)
+    http.post(urlToFetch, answerData)
         .then(data => {
-            if(data.message === 'Correct answer!'){
+            if (data.message === 'Correct answer!') {
                 console.log('YEEEEEYYYY')
                 console.log(data)
-                //urlToFetch = data.nextURL
-                fetchNextQuestion(data.nextURL)
+                if ( typeof data.nextURL !== 'undefined') {
+                    fetchQuestion(data.nextURL)
+                } else {
+                    ui.drawLastQuestion()
+                    console.log('THIS WAS THE LAST QUESTIONNNN')
+                }
+
             } else {
                 console.log('NOT RIGHT ANSWER, YOU SUCKER!')
                 console.log(data)
             }
-            
+
         })
-        .catch(err => console.log(err)) 
+        .catch(err => console.log(err))
 }
--- ui.js
+++ ui.js
@@ -1,25 +1,22 @@
 class UI {
-    // Make a http get request
     draw(data) {
         let output = '';
         output += `<div><h5>${data.question}</h5></div>`
-        // console.log(data.alternatives)
-        
-        if (data.alternatives === undefined){
-            output += `<input type="text"></input>`
-            //console.log('IT HAS NO ALTERNATIVES')
-        } else{
+        if (data.alternatives === undefined) {
+            output += `<input type="text" id="answer-text"></input>`
+        } else {
             for (var prop in data.alternatives) {
-                //output += `<input type="radio" name="answer" value=${data.alternatives[prop]}>`
                 output += `<input type="radio" name="answer" value=${prop}>`
                 output += `<label for="${data.alternatives[prop]}">${data.alternatives[prop]}</label>`
-              }
-            //console.log('IT HAS ALTERNATIVES')
+            }
         }
-        
         document.getElementById('output').innerHTML = output
-       
     }
 
-    
+    drawLastQuestion(){
+        let output = '';
+        output += `<div><h5>Congratulations! You cleared all the questions...</h5></div>`
+        output += `<button id="button2">Show Results</button>`
+        document.getElementById('output').innerHTML = output
+    }
 }
