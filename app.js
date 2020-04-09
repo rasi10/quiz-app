1d050fa454fe58b01a355b843849ec9d6d6a0f2d
Rafael Silva <rafael@Rafaels-MacBook-Pro.local>
2020-04-09 23:58:59 +0200

    completed the flow of the quiz

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const http = new EasyHTTP
 const ui = new UI
+
 let urlToFetch = ''
 let urlToSendAnswer = ''
+let playerName = ''
 
-document.querySelector('#button1').addEventListener('click', startQuiz)
-document.querySelector('#button2').addEventListener('click', submitAnswer)
+startUI()
+
+function startUI() {
+    ui.drawStart()
+    document.querySelector('#button1').addEventListener('click', startQuiz)
+}
 
 function startQuiz() {
+    playerName = document.querySelector('#name-text').value
     urlToFetch = 'http://vhost3.lnu.se:20080/question/1'
     fetchQuestion(urlToFetch)
 }
@@ -15,7 +22,8 @@ function fetchQuestion(url) {
     console.log(url)
     http.get(url)
         .then(data => {
-            ui.draw(data)
+            ui.drawQuestion(data, playerName)
+            document.querySelector('#button2').addEventListener('click', submitAnswer)
             console.log(data)
             urlToFetch = data.nextURL
         })
@@ -36,10 +44,11 @@ function submitAnswer() {
             if (data.message === 'Correct answer!') {
                 console.log('YEEEEEYYYY')
                 console.log(data)
-                if ( typeof data.nextURL !== 'undefined') {
+                if (typeof data.nextURL !== 'undefined') {
                     fetchQuestion(data.nextURL)
                 } else {
                     ui.drawLastQuestion()
+                    document.querySelector('#button3').addEventListener('click', showResults)
                     console.log('THIS WAS THE LAST QUESTIONNNN')
                 }
 
@@ -47,7 +56,30 @@ function submitAnswer() {
                 console.log('NOT RIGHT ANSWER, YOU SUCKER!')
                 console.log(data)
             }
-
         })
         .catch(err => console.log(err))
+}
+
+function showResults() {
+    updateResultsList()
+    showRankingList()
+    document.querySelector('#button4').addEventListener('click', startUI)
+}
+
+function updateResultsList() {
+    const scores = JSON.parse(window.localStorage.getItem('scores') || '[]')
+    scores.push({
+        name: playerName,
+        time: 10
+    })
+    window.localStorage.setItem('scores', JSON.stringify(scores))
+}
+
+function showRankingList() {
+    var savedScores = window.localStorage.getItem('scores')
+    var scoresAsJson = JSON.parse(savedScores)
+    scoresAsJson.sort(function (a, b) {
+        return a.time - b.time
+    })
+    ui.drawResults(scoresAsJson)
 }
--- ui.js
+++ ui.js
@@ -1,6 +1,8 @@
 class UI {
-    draw(data) {
+    drawQuestion(data, playerName) {
         let output = '';
+        output += `<h1>Quiz Application</h1>`
+        output += `<h4>Welcome, <spam id="player-name">${playerName}</spam></h4>`
         output += `<div><h5>${data.question}</h5></div>`
         if (data.alternatives === undefined) {
             output += `<input type="text" id="answer-text"></input>`
@@ -10,13 +12,46 @@ class UI {
                 output += `<label for="${data.alternatives[prop]}">${data.alternatives[prop]}</label>`
             }
         }
-        document.getElementById('output').innerHTML = output
+        output+=`<button id="button2">Submit answer</button>`
+        document.getElementById('container').innerHTML = output
+    }
+
+    drawStart(){
+        let output = ''
+        output += `<h1>Quiz Application</h1>`
+        output += `<h5>Please, enter your name</h5>`
+        output += `<input type="text" id="name-text"></input>`
+        output += `<button id="button1">Start The Quiz</button>`
+        document.getElementById('container').innerHTML = output
+
     }
 
     drawLastQuestion(){
         let output = '';
+        output += `<h1>Quiz Application</h1>`
         output += `<div><h5>Congratulations! You cleared all the questions...</h5></div>`
-        output += `<button id="button2">Show Results</button>`
-        document.getElementById('output').innerHTML = output
+        output += `<button id="button3">Show Results</button>`
+        document.getElementById('container').innerHTML = output
     }
+
+    drawResults (webstorageData) {
+        let output = '';
+        output += `<h1>Quiz Application</h1>`
+        output += `<div><h5>Ranking list</h5></div>`
+        output += `<table>
+                        <tr>
+                            <th>Player</th>
+                            <th>Time(secs)</th>
+                        </tr>`
+    
+        for (let index = 0; (index < webstorageData.length && index < 5); index++) {
+            output += `<tr>
+                            <td>${webstorageData[index].name}</td>
+                            <td>${webstorageData[index].time}</td>
+                        </tr>`
+        }
+        output += `</table>`
+        output += `<button id="button4">Restart</button>`
+        document.getElementById('container').innerHTML = output    
+        }        
 }
